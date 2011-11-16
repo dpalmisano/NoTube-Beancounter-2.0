bd2e3873c86ce3562dfe19542a025e0f9b4bee3a
Davide Palmisano <dpalmisano@gmail.com>
2011-11-16 14:18:23 +0000

    refactor ended

--- commons/storage/model/src/main/java/tv/notube/commons/alog/ActivityLogException.java
+++
@@ -1,9 +0,0 @@
-package tv.notube.commons.alog;
-
-/**
- * put class description here
- *
- * @author Davide Palmisano ( dpalmisano@gmail.com )
- */
-public class ActivityLogException extends Exception {
-}
